b90754c88eea94814d56852c1ef93b9c30e1cdc9
Jeff Lindsay <progrium@gmail.com>
2012-03-08 06:26:26 -0800

    dropped all mention of websocket

--- gtutorial/gateway.py
+++ gtutorial/gateway.py
@@ -7,7 +7,6 @@ from ginkgo.config import Setting
 
 from .numbers import NumberClient
 from .messaging.hub import MessageHub
-from .messaging.websocket import WebSocketStreamer
 
 logger = logging.getLogger(__name__)
 
@@ -15,10 +14,8 @@ class NumberGateway(Service):
     def __init__(self):
         self.client = NumberClient(('127.0.0.1', 7776))
         self.hub = MessageHub()
-        self.ws = WebSocketStreamer(self.hub)
 
         self.add_service(self.hub)
-        self.add_service(self.ws)
         self.add_service(self.client)
 
     def do_start(self):
