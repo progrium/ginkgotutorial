7f595555fddea8b6ed3520d62a2f2805e0d3fcbb
Jeff Lindsay <progrium@gmail.com>
2012-03-09 01:51:21 -0800

    dropped unnecessary configuration from later chapters

--- config/gateway.conf.py
+++ config/gateway.conf.py
@@ -1,7 +1,3 @@
-import os
-
-identity = os.environ.get('IDENTITY', '127.0.0.1')
-cluster = os.environ.get('CLUSTER', '127.0.0.1').split(',')
 
 def service():
     from gtutorial.gateway import NumberGateway
